f9323561119ffc7fd9b9394526abdc6d03ab851c
NrUnoDos <ullmann.jonas1946@gmail.com>
2025-09-07 19:47:08 +0200

    Improve dark mode handling

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
         <html lang="de">
         <body className="min-h screen flex flex-col">
         <main className="flex-1">{children}</main>
-        <footer className="border-t border-gray-200 bg-white">
-            <div className="mx-auto max-w-5xl px-4 py-4 flex items-center justify-center gap-6 text-sm text-gray-700">
+        <footer className="border-t border-gray-300">
+            <div className="mx-auto max-w-5xl px-4 py-4 flex items-center justify-center gap-6 text-sm">
                 <Link href="/" className="hover:underline">Home</Link>
                 <Link href="/impressum" className="hover:underline">Impressum</Link>
                 <Link href="/datenschutz" className="hover:underline">Datenschutz</Link>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     const [activeTab, setActiveTab] = useState('merge');
     return (
         <div
-            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
             <div className="flex gap-2 w-full border-b border-gray-200">
                 <button
                     className={`px-4 py-2 ${activeTab === 'merge' ? 'border-b-2 border-gray-800' : ''}`}
